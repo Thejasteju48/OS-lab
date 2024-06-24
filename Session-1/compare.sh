f426ef49031b7380914177c16ca77de172ee89c5
Thejas RM <thejast397@gmail.com>
2024-06-24 21:38:17 +0530

    Update compare.sh

--- Session-1/compare.sh
+++ Session-1/compare.sh
@@ -5,7 +5,7 @@ read str1
 read str2
 if [ $str1 == $str2 ]; then
   echo "Strings are equal"
-elif [ $str1 \> $str2 ]; then
+elif [ $str1 -gt $str2 ]; then
   echo "String1 is greater than string2"
 else
   echo "String1 is lesser than string2"
